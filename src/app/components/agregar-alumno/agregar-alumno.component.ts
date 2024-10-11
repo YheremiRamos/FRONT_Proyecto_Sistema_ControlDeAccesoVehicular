dc856caa70ce59f51d24865c27b2327a772002ec
YheremiRamos <yheremiramosquispe@gmail.com>
2024-10-11 08:46:21 -0500

    MensajesFin

--- src/app/components/agregar-alumno/agregar-alumno.component.ts
+++ src/app/components/agregar-alumno/agregar-alumno.component.ts
@@ -16,87 +16,95 @@ import { Usuario } from '../../models/usuario.model';
   templateUrl: './agregar-alumno.component.html',
   styleUrls: ['./agregar-alumno.component.css']
 })
-export class AgregarAlumnoComponent implements OnInit{
- 
-    dataSource:any; 
+export class AgregarAlumnoComponent implements OnInit {
+  
+  dataSource: any;
+  filtro: string = '';
+  varDni: string = '';
+  objUsuario: Usuario = {};
 
-    filtro: string = "";
-    varDni : string = "";
-    objUsuario: Usuario  = {};
+  nombres = '';
+  apellidos = '';
+  habilitarRegistrar: boolean = false;
 
-    nombres = "";
-    apellidos = "";
-    habilitarRegistrar: boolean = false;
-
-  formRegistra = this.formBuilder.group({ 
-    validaDni: ['', [Validators.required, Validators.min(8), Validators.pattern('^[0-9]+$')] ] , 
-    validaTipoVehiculo: ['', Validators.min(1)] , 
-    validaPlaca:  ['', [Validators.required, Validators.pattern('^[A-Z]{2}-\\d{3,5}$') ]] ,  // Formato: AA-1234 o AA-12345
-    nombres: [{value: '', disabled: true}] ,  // Agregar campo de nombre
-    apellidos: [{value: '', disabled: true}] ,  // Agregar campo de apellidos
+  formRegistra = this.formBuilder.group({
+    validaDni: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^[0-9]+$')]],
+    validaTipoVehiculo: ['', Validators.min(1)],
+    validaPlaca: ['', [Validators.required, Validators.pattern('^[A-Z]{2}-\\d{3,5}$')]], // Formato: AA-1234 o AA-12345
+    nombres: [{ value: '', disabled: true }],
+    apellidos: [{ value: '', disabled: true }],
   });
 
   constructor(
     private tokenService: TokenService,
-    private usuarioService:UsuarioService,
-    private formBuilder: FormBuilder) 
-  {
+    private usuarioService: UsuarioService,
+    private formBuilder: FormBuilder
+  ) {
     this.objUsuario.idUsuario = this.tokenService.getUserId();
-    console.log("constructor >> constructor >>> " + this.tokenService.getToken());
+    console.log('constructor >> constructor >>> ' + this.tokenService.getToken());
   }
 
-  buscarPorDni(){
-    console.log(">>> Filtrar EXCEL [ini]");
-    console.log(">>> varDni: "+ this.varDni);
+  buscarPorDni() {
+    console.log('>>> Filtrar EXCEL [ini]');
+    console.log('>>> varDni: ' + this.varDni);
 
-    this.usuarioService.buscarUsuarioDni(
-      this.varDni
-      ).subscribe(
-        (x) => {
-          this.dataSource = x;
-    
-          // Asegurarse de que los datos existan antes de usarlos
-          if (this.dataSource && this.dataSource.length > 0) {
-            const usuario = this.dataSource[0]; // Si es una lista, accede al primer usuario
-            
-            // Llenar los campos del formulario con los datos traídos
-            this.formRegistra.patchValue({
-              nombres: usuario.nombres,
-              apellidos: usuario.apellidos
-            });
-            this.habilitarRegistrar = false;
-            console.log(">>> data: " + usuario.nombres);
-          } else {
-            console.log(">>> Usuario no encontrado");
-            // Limpiar los campos si no hay resultados
-            this.formRegistra.patchValue({
-              nombres: '',
-              apellidos: ''
-            });
-          }
-        },
-        (error) => {
-          console.log(">>> Error al buscar por DNI: ", error);
-          this.limpiarFormulario();
+    this.usuarioService.buscarUsuarioDni(this.varDni).subscribe(
+      (x) => {
+        this.dataSource = x;
+
+        if (this.dataSource && this.dataSource.length > 0) {
+          const usuario = this.dataSource[0];
+          this.formRegistra.patchValue({
+            nombres: usuario.nombres,
+            apellidos: usuario.apellidos,
+          });
+          this.habilitarRegistrar = false;
+        } else {
+          // Mostrar alerta si no encuentra al usuario
+          Swal.fire('Por favor registrar los nuevos datos del propietario.');
           this.habilitarRegistrar = true;
+          this.formRegistra.get('nombres')?.enable();
+          this.formRegistra.get('apellidos')?.enable();
+          this.formRegistra.patchValue({
+            nombres: '',
+            apellidos: '',
+          });
         }
-      );
-    
-      console.log(">>> Filtrar [fin]");
-    }
+      },
+      (error) => {
+        console.log('>>> Error al buscar por DNI: ', error);
+        this.limpiarFormulario();
+        this.habilitarRegistrar = true;
+      }
+    );
 
-    limpiarFormulario() {
-      this.formRegistra.patchValue({
-        nombres: '',
-        apellidos: ''
-      });
-    }
-  registra(){
+    console.log('>>> Filtrar [fin]');
+}
 
+registrarDatos() {
+    const datos = this.formRegistra.value;
+    Swal.fire({
+      title: 'Confirmación de Registro',
+      text: `DNI: ${this.varDni}, Nombres: ${datos.nombres}, Apellidos: ${datos.apellidos}, Placa: ${datos.validaPlaca}, Tipo: ${datos.validaTipoVehiculo}`,
+      icon: 'info',
+    });
+
+    console.log('>>> Filtrar [fin]');
   }
 
-  ngOnInit(): void{
-    
+  limpiarFormulario() {
+    this.formRegistra.patchValue({
+      nombres: '',
+      apellidos: '',
+    });
   }
 
+  registra() {
+    // Lógica para registrar al usuario/vehículo
+    console.log('Registro exitoso.');
+  }
+
+  ngOnInit(): void {
+    // Implementación si es necesaria durante la inicialización del componente
+  }
 }
